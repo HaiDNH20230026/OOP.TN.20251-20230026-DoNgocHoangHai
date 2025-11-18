782c7270842ccbb0fb0f9500904ee03173380edc
Đỗ Ngọc Hoàng Hải <hai.dnh230026@sis.hust.edu.vn>
2025-11-18 16:05:33 +0700

    Book and BookTest

---
+++ AimsProject/AimsProject/src/com/hust/kstn/models/Book.java
@@ -0,0 +1,56 @@
+package com.hust.kstn.models;
+
+import java.util.*;
+
+public class Book {
+    
+    private int id;
+    private String title;
+    private String category;
+    private double cost;
+    private int numOfTokens;
+    private List<BookAuthor> authors = new ArrayList<>();
+
+    public Book(int id, String title, String category, double cost, int numOfTokens, List<BookAuthor> authors) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.numOfTokens = numOfTokens;
+        this.authors = authors;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getNumOfTokens() {
+        return numOfTokens;
+    }
+
+    public List<BookAuthor> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [" + this.id + "][" 
+             + this.title + "] [" 
+             + this.cost + "][" 
+             + this.category + "][" 
+             + this.numOfTokens + "]" 
+             + "\nAuthors: " + this.authors;
+    }
+}
---
+++ AimsProject/AimsProject/src/com/hust/kstn/test/BookTest.java
@@ -0,0 +1,32 @@
+package com.hust.kstn.test;
+
+import com.hust.kstn.models.Book;
+import com.hust.kstn.models.BookAuthor;
+import java.util.*;
+
+public class BookTest {
+    public static void main(String[] args) {
+        BookAuthor author1 = new BookAuthor("Joshua Bloch", 1961, "American software engineer");
+        BookAuthor author2 = new BookAuthor("Neal Gafter", 1960, "Software architect");
+
+        List<BookAuthor> authors = new ArrayList<>();
+        authors.add(author1);
+        authors.add(author2);
+
+        Book effectiveJava = new Book(
+            1,
+            "Effective Java",
+            "Programming",
+            45.0,
+            50000,
+            authors
+        );
+
+        System.out.println("--- Testing Book.toString() ---");
+        System.out.println(effectiveJava);
+        
+        System.out.println("\n--- Testing getters ---");
+        System.out.println("Book Title: " + effectiveJava.getTitle());
+        System.out.println("First Author: " + effectiveJava.getAuthors().get(0).getFullName());
+    }
+}
